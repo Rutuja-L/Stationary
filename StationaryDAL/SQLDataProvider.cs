3bef1007704d3c5589ce7d18bc3c50879e9c8007
Rutuja-L <rutu.loke@gmail.com>
2017-02-27 17:06:11 +0530

    StationeryDAL

---
+++ StationaryDAL/SQLDataProvider.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StationaryDAL
+{
+    public class SQLDataProvider
+    {
+        private string ConnectionString { get; set; }
+
+        public SQLDataProvider(string connString)
+        {
+            ConnectionString = connString;
+        }
+
+
+        public int GetSample()
+        {
+            int rtnVal=0;
+            SqlConnection conn = new SqlConnection(ConnectionString);
+
+            return rtnVal;
+        }
+
+
+
+    }
+}
